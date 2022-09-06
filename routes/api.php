debb2e60b29cc1dca689235cfb18b13f0ce28814
unknown <vikrant@oliveindesign.com>
2022-09-06 11:18:24 +0530

    import controller in api route

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\api\App_controller;
-// use App\Http\Controllers\api\Auth_controller;
+use App\Http\Controllers\api\App_controller;
+use App\Http\Controllers\api\Auth_controller;
 /*
 |--------------------------------------------------------------------------
 | API Routes
