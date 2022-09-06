c4650253fa7a481f9d95fafa6727ecdc055c95b4
unknown <vikrant@oliveindesign.com>
2022-09-06 11:33:41 +0530

    add slash in rout

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\api\Auth_controller;
 */
 
 
-Route::get('greeting', [Auth_controller::class, 'greeting']);
+Route::get('/greeting', [Auth_controller::class, 'greeting']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
