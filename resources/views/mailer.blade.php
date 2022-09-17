8e2b12a6a9e972bb9859405b822bb7cd40a27ac0
unknown <vikrant@oliveindesign.com>
2022-09-17 11:37:09 +0530

    email test

--- resources/views/mailer.blade.php
+++ resources/views/mailer.blade.php
@@ -1,14 +1,14 @@
 
-<!-- <!DOCTYPE html
+<!DOCTYPE html
 PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml"> -->
+<html xmlns="http://www.w3.org/1999/xhtml">
 <html>
     
     <head>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;0,900;1,800&display=swap"
     rel="stylesheet">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
     <title>Xsent</title>
 
 </head> 
