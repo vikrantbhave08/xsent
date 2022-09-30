4fc33a702d8ea70551550331ef78c878d1358244
unknown <vikrant@oliveindesign.com>
2022-09-30 15:35:10 +0530

    chn

--- resources/views/pdf_page.blade.php
+++ resources/views/pdf_page.blade.php
@@ -4,44 +4,39 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-      
+        <title>PDF</title>
 
         <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
 
-            .ndfHFb-c4YZDc-Wrql6b{
-                display:none;
-            }
         </style>
     </head>
-    <body class="antialiased">  
+    <body>  
 
-    <div style="width: 100%; height: 800px; position: relative;"> 
+    <!-- <div style="width: 100%; height: 800px; position: relative;"> 
  
 
       <iframe src="https://docs.google.com/gview?url={{ $pdf_url }}&embedded=true"   width="100%" height="800px" frameborder="0"></iframe>
 
       <div style="width: 50px; height: 50px; position: absolute;right: 12px; top: 12px; background:#fff"> </div>
-</div>
+</div> -->
 
     
-    <!-- <iframe sandbox="allow-scripts allow-same-origin" 
-      id="myDiv" src="https://docs.google.com/gview?url={{ $pdf_url }}&embedded=true" style="width:100%; height:800px;" frameborder="0"></iframe> -->
+    <iframe sandbox="allow-scripts allow-same-origin" 
+      id="myDiv" src="https://docs.google.com/gview?url={{ $pdf_url }}&embedded=true" style="width:100%; height:800px;" frameborder="0"></iframe>
    
 
- <script>
+<script type='text/javascript'>
+    setTimeout(function() {
+        var elems = document.getElementsByClassName('ndfHFb-c4YZDc-Wrql6b');
+        for (var i=0;i<elems.length;i+=1){
+            alert();
+        elems[i].style.display = 'none';
+        }
+        // document.getElementsByClassName("ndfHFb-c4YZDc-Wrql6b").style.display = 'none';    
+    }, 6000);
 
-    </script>
-
-
-                   
+</script>
+                  
 
                    
     </body>
