1f2c068790e6b6510a0e4f01117811794554a8b3
unknown <vikrant@oliveindesign.com>
2022-10-13 12:14:00 +0530

    monthly report

--- app/Http/Controllers/Mobile_api/App_controller.php
+++ app/Http/Controllers/Mobile_api/App_controller.php
@@ -91,7 +91,7 @@ class App_controller extends Controller
             $spend_analysys_by_cat=Shop_transaction_model::select('shop_transactions.*',DB::raw('ifnull(SUM(shop_transactions.amount),0) as total_sale'))
                                                     ->where(function ($query) use ($request,$logged_user,$cat,$shops) {                                                   
                                                     $query->whereIn('shop_transactions.shop_id', $shops);  //shops related transaction
-                                                    if ($logged_user['user_role']==3 && !empty($request['user_id'])) $query->where('shop_transactions.by_user',$request['user_id']);  //for child data when parent is accessing  for category related on billing history page                                               
+                                                    if ($logged_user['user_role']==3 && !empty($request['user_id']) !empty($request['billing_history'])) $query->where('shop_transactions.by_user',$request['user_id']);  //for child data when parent is accessing  for category related on billing history page                                               
                                                     if ($logged_user['user_role']==4) $query->where('shop_transactions.by_user',$logged_user['user_id']);  //for child data when self accessing                                                   
                                                     if ($request['spend_for']=='children' && empty($request['user_id'])) $query->where('shop_transactions.by_user', $cat);  //user related transaction
                                                     if (!empty($request['year'])) $query->whereYear('shop_transactions.created_at', '=', $request['year']);
