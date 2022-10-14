da70a04b38a25e6d567f3c9a0f9ee8eaaeb216d1
unknown <vikrant@oliveindesign.com>
2022-10-14 11:35:36 +0530

    add complaintid in complaints

--- database/migrations/2022_10_14_100441_add_columns_to_complaints_table.php
+++ database/migrations/2022_10_14_100441_add_columns_to_complaints_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddComplaintIdToComplaintsTable extends Migration
+class AddColumnsToComplaintsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddComplaintIdToComplaintsTable extends Migration
     public function up()
     {
         Schema::table('complaints', function (Blueprint $table) {
-         
+            $table->string('complaintid',50)->default(0)->after('complaint_id'); 
         });
     }
 
