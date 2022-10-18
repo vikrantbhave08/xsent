74ed6d102ec9942878f7dd0c7b2c07ec13a77841
unknown <vikrant@oliveindesign.com>
2022-10-18 14:10:28 +0530

    demo.tx

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport; // add this
 
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes(); // Add this
+
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
+        Passport::hashClientSecrets();
 
         //
     }
